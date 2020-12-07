df19f91323ba1c911c77a856b8cb9014df6b6726
wuyinghui <2033345759@qq.com>
2020-12-07 10:21:59 +0800

    主干添加

--- HelloGit/src/main/java/cn/itheima/git/HelloGit.java
+++ HelloGit/src/main/java/cn/itheima/git/HelloGit.java
@@ -10,5 +10,6 @@ public class HelloGit {
         System.out.println("更新1..");
         System.out.println("更新2..");
         System.out.println("创建分支");
+        System.out.println("主干添加0.0.0.0");
     }
 }
